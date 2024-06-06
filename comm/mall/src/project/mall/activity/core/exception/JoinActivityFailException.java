b4cad6e56e40b5de1d1b249f4c9104b6a051e72d
hehao <poloo852@outlook.com>
2024-06-06 16:22:30 +0800

    删除activity模块

--- comm/mall/src/project/mall/activity/core/exception/JoinActivityFailException.java
+++
@@ -1,8 +0,0 @@
-package project.mall.activity.core.exception;
-
-public class JoinActivityFailException extends RuntimeException {
-
-    public JoinActivityFailException(String errMsg) {
-        super(errMsg);
-    }
-}
